000bfc675da306ee649d27a93b2c7c0cab86e0dd
Deepak Kumar <deepak.2002.02.16@gmail.com>
2022-10-07 14:01:35 +0530

    opcode string to u8 decode

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::num::ParseIntError;
 
+fn decode(s: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..(s.len() - 1))
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .collect()
+}
 fn run() -> Result<(), Box<dyn Error>> {
     let filename = "solidity/contract.bin";
     let mut f = File::open(filename)?;
     let mut buffer = String::new();
     f.read_to_string(&mut buffer)?;
-    println!("{}", buffer);
+    println!("{}\n", buffer);
+    let bytecode = decode(&buffer)?;
+    for b in bytecode {
+        print!("0x{:x}", b);
+    }
     Ok(())
 }
 
