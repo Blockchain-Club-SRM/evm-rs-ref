6fd1bf12c4b83c9d14a48682d564e87f90cdbd73
Deepak Kumar <deepak.2002.02.16@gmail.com>
2022-10-07 13:54:41 +0530

    intial fil i/o opperations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let filename = "solidity/contract.bin";
+    let mut f = File::open(filename)?;
+    let mut buffer = String::new();
+    f.read_to_string(&mut buffer)?;
+    println!("{}", buffer);
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    run().unwrap();
 }
